3eb573e0e4e3695dd08950b3958837d4bdcd3fbc
emrubio85 <119766137+emrubio85@users.noreply.github.com>
2025-11-12 18:46:55 +0000

    etiquetas de para mui

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,27 @@ function App() {
         <h1>Bienvenido al Dashboard</h1>
       </div>
 
-      <Grid>
+      <Grid container spacing={5} justifyContent="center" alignItems="center">
         {/* Encabezado */}
-        <Grid>Elemento: Encabezado</Grid>
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
 
         {/* Alertas */}
-        <Grid>Elemento: Alertas</Grid>
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
 
         {/* Selector */}
-        <Grid>Elemento: Selector</Grid>
+        <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
 
         {/* Indicadores */}
-        <Grid>Elemento: Indicadores</Grid>
+        <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
 
         {/* Gráfico */}
-        <Grid>Elemento: Gráfico</Grid>
+        <Grid sx={{ display: { xs: "none", md: "block"} }} size={{ xs: 12, md: 6 }}>Elemento: Gráfico</Grid>
 
         {/* Tabla */}
-        <Grid>Elemento: Tabla</Grid>
+        <Grid sx={{ display: { xs: "none", md: "block"} }} size={{ xs: 12, md: 6 }}>Elemento: Tabla</Grid>
 
         {/* Información adicional */}
-        <Grid>Elemento: Información adicional</Grid>
+        <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
       </Grid>
     </>
   )
